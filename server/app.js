eb2148007ef2fbb2a720f8ab5d2975391e47ae20
danoGuerrero <danielguerrerodev@gmail.com>
2023-07-13 14:18:26 -0600

    cors

--- server/app.js
+++ server/app.js
@@ -6,18 +6,6 @@ const routes = require('./routes/productsRoutes');
 const categoriesRoutes = require('./routes/categoriesRoutes');
 
 app.use(routes, cors());
-app.use(function (req, res, next) {
-	req.header("Access-Control-Allow-Origin", "*");
-	req.header(
-		"Access-Control-Allow-Methods",
-		"GET,HEAD,OPTIONS,POST,PUT,DELETE"
-	);
-	req.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
-	);
-	next();
-});
 app.use(categoriesRoutes);
 
 module.exports = app;
