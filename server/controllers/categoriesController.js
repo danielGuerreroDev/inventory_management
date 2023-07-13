247dbb7fa834b442e833806cbe7f03df2dc24405
danoGuerrero <danielguerrerodev@gmail.com>
2023-07-13 11:51:21 -0600

    headers

--- server/controllers/categoriesController.js
+++ server/controllers/categoriesController.js
@@ -5,6 +5,12 @@ exports.getCategories = async (req, res) => {
     try {
         const categories = await Categories.find({});
         res.send(categories);
+        res.status(200).json({
+          body: "Hello, world!",
+          headers: {
+            "access-control-allow-origin": "*",
+          },
+      });
       } catch (err) {
         console.log(err);
       }
