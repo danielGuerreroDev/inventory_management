f7fb47202c48047c130afb4d978f52b11fe8208f
danoGuerrero <danielguerrerodev@gmail.com>
2023-07-14 12:52:27 -0600

    cors

--- server/routes/productsRoutes.js
+++ server/routes/productsRoutes.js
@@ -11,6 +11,7 @@ router.get('/', productsController.baseRoute);
 router.get('/getProducts',cors() , productsController.getProducts);
 router.get('/getProduct/:id',cors() , productsController.getSingleProduct);
 router.put('/product/:id/update',cors({
+    origin: ['https://prod--im-danielguerrero.netlify.app'],
     methods: ['POST', 'PUT', 'UPDATE', 'OPTIONS']
 }) , jsonParser, productsController.updateProduct);
 router.delete('/product/delete/:id',cors() , productsController.deleteProduct);
