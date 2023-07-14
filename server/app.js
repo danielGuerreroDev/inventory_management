37eef23b1db771fe93ace0461f92f0a0dffa527a
danoGuerrero <danielguerrerodev@gmail.com>
2023-07-14 13:33:42 -0600

    cors

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const categoriesRoutes = require('./routes/categoriesRoutes');
 app.use(routes, cors({
     origin: 'https://prod--im-danielguerrero.netlify.app/',
     optionsSuccessStatus: 200,
-    methods: "GET, PUT"
+    methods: ["GET", "PUT", "UPDATE", "DELETE", "OPTIONS"]
 }));
 app.use(categoriesRoutes);
 
