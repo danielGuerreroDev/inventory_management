1e50f8b556869e6b0c198c79eeb5b4d15e810e78
danoGuerrero <danielguerrerodev@gmail.com>
2023-07-18 13:53:36 -0600

    cors

--- server/routes/productsRoutes.js
+++ server/routes/productsRoutes.js
@@ -1,4 +1,4 @@
-const cors = require('cors');
+// const cors = require('cors');
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
@@ -8,17 +8,9 @@ const jsonParser = bodyParser.json();
 const productsController = require('../controllers/productsController');
 
 router.get('/', productsController.baseRoute);
-router.get('/getProducts', cors({
-    origin: 'https://prod--im-danielguerrero.netlify.app',
-    optionsSuccessStatus: 200,
-    methods: ["GET"]
-}), productsController.getProducts);
-router.get('/getProduct/:id', cors({
-    origin: 'https://prod--im-danielguerrero.netlify.app',
-    optionsSuccessStatus: 200,
-    methods: ["GET"]
-}), productsController.getSingleProduct);
-router.put('/product/:id/update', cors(), jsonParser, productsController.updateProduct);
+router.get('/getProducts', productsController.getProducts);
+router.get('/getProduct/:id', productsController.getSingleProduct);
+router.put('/product/:id/update', jsonParser, productsController.updateProduct);
 router.delete('/product/delete/:id', productsController.deleteProduct);
 
 
