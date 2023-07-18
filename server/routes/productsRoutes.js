ca08719d60c0cc5b1c43a0999981ba858716fa3c
danoGuerrero <danielguerrerodev@gmail.com>
2023-07-18 12:23:15 -0600

    cors

--- server/routes/productsRoutes.js
+++ server/routes/productsRoutes.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
@@ -7,8 +8,8 @@ const jsonParser = bodyParser.json();
 const productsController = require('../controllers/productsController');
 
 router.get('/', productsController.baseRoute);
-router.get('/getProducts', productsController.getProducts);
-router.get('/getProduct/:id', productsController.getSingleProduct);
+router.get('/getProducts', cors, productsController.getProducts);
+router.get('/getProduct/:id', cors, productsController.getSingleProduct);
 router.put('/product/:id/update', jsonParser, productsController.updateProduct);
 router.delete('/product/delete/:id', productsController.deleteProduct);
 
