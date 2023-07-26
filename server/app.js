943d1b395e1d3e50832236428ad9bfdc3e31090c
danoGuerrero <danielguerrerodev@gmail.com>
2023-07-25 20:24:42 -0600

    cors

--- server/app.js
+++ server/app.js
@@ -12,13 +12,12 @@ app.use(routes);
 app.options('/product/delete/:id', cors({
     origin: "https://inventory-management-net.onrender.com",
     methods: "OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
+    preflightContinue: true,
     optionsSuccessStatus: 204
 }));
 app.delete('/product/delete/:id', cors({
     origin: "https://inventory-management-net.onrender.com",
     methods: "OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: true,
     optionsSuccessStatus: 204
 }
 ), function (req, res, next) {
